0116118f85cb66ba5c121ae6e458f95d298b677e
SoumyaS09 <shetty.soumya09@yahoo.in>
2024-02-27 14:47:42 +0530

    Create Stud.java

---
+++ Student_Debate/src/main/java/com/greatlearning/entity/Stud.java
@@ -0,0 +1,61 @@
+package com.greatlearning.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Student")
+public class Stud {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String fullName;
+
+	private String department;
+
+	private String country;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Stud [id=" + id + ", fullName=" + fullName + ", department=" + department + ", country=" + country
+				+ "]";
+	}
+
+}
